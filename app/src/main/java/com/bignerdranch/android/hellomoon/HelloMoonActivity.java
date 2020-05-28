861a89c2fb73e2af7a90a1a07972715ed05ce56b
wylovelc <wy01415@126.com>
2020-05-28 16:16:50 +0800

    第13章完成

--- app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
@@ -1,9 +1,10 @@
 package com.bignerdranch.android.hellomoon;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HelloMoonActivity extends AppCompatActivity {
+public class HelloMoonActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.hellomoon;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+
+    private AudioPlayer mPlayer = new AudioPlayer();
+
+    private Button mPlayButton;
+    private Button mStopButton;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon,parent,false);
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.play(getActivity());
+            }
+        });
+
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+
+}
