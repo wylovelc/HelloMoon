019d6d67079c677d39278b9b7db902ca63e0c287
wylovelc <wy01415@126.com>
2020-05-28 15:05:24 +0800

    新项目

--- app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
+++ app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonActivity.java
@@ -3,11 +3,11 @@ package com.bignerdranch.android.hellomoon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HelloMoonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello_moon);
     }
 }
